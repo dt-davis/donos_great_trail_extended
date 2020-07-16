23accfeda3cfa6f7baea5bae26d1be680d742fee
Donovan Davis <dt-davis@Hotmail.com>
2020-07-16 16:04:29 -0500

    finished to rubric

--- assests/scripts/traveler.js
+++ assests/scripts/traveler.js
@@ -18,3 +18,41 @@ class Traveler {
     }
 
 }
+
+class Doctor extends Traveler {
+    constructor (name){
+        super(name)
+    }
+
+    heal(Traveler){
+        Traveler.isHealthy = true
+    }
+}
+
+class Hunter extends Traveler {
+    constructor (name){
+        super(name)
+        this.food = 2
+    }
+    
+    eat(){
+        if(this.food < 2){
+            console.log(this.name + ' is no longer healthy')
+            this.isHealthy = false
+            this.food = 0
+        }else this.food -= 2
+    }
+    
+    hunt (){
+        this.food += 5
+    }
+
+    giveFood (Traveler, numOfFoodUnits){
+        if(this.food < numOfFoodUnits){
+            console.log(this.name + ' doesnt have that much food')
+        }else {
+            this.food -= numOfFoodUnits
+            Traveler.food += numOfFoodUnits
+        }
+    }
+}
